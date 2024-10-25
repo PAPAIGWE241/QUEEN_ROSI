1e05a31627934b048841cb059e4fa0046bb03551
PAPAIGWE241 <techigwe@gmail.com>
2024-10-25 22:57:25 +0100

    Update koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ RUN sudo dnf -y update &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
     sudo dnf clean all -y
 
-RUN git https://github.com/salmanytofficial/XLICON-V2-MD
+RUN git https://github.com/PAPAIGWE241/QUEEN_ROSI
 
-WORKDIR /root/XLICON-V2-MD
+WORKDIR /root/QUEEN_ROSI
 
-COPY ./root/XLICON-V2-MD
+COPY ./root/QUEEN_ROSI
 
 RUN yarn install
 
