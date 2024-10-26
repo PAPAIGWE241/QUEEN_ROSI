fdb3b7bafffbc2ce82d419367e5cd35307818730
PAPAIGWE241 <techigwe@gmail.com>
2024-10-26 08:34:10 +0100

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,14 +1,14 @@
 let handler = async m => m.reply(`
 
-╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
-│ 📂 *BOT Name:* _XLICON-V2-MD_
-│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
-│ 👤 *Owner:* _Salman Ahmad_
-│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e
+╭⭑⭑⭑★✪ *QUEEN•ROSI* ✪★⭑⭑⭑
+│ 📂 *BOT Name:* _QUEEN-ROSI-MD_
+│ 📝 *Description:* _I'm Queen rosi bby. A MultiDevice WhatsApp bot with rich features Created By PAPA IGWE TECH inc._
+│ 👤 *Owner:* _PAPA IGWE_
+│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaoRh0h2UPBAyRobHB2t
 ╰━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['channel']
 handler.tags = ['main']
-handler.command = ['xchannel', 'channel' ] 
+handler.command = ['igwechannel', 'channel' ] 
 
 export default handler
